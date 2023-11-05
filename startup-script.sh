28d52b6b8f588244f1d2aaaf74fd9017d43470e5
FadyAtefElyas <fadyatefelyas@gmai.com>
2023-11-05 03:55:08 +0200

    modify start-up

--- startup-script.sh
+++ startup-script.sh
@@ -4,22 +4,13 @@
    curl -fsSL https://get.docker.com -o get-docker.sh
    sudo sh get-docker.sh
    
-   # install kubectl 
-   sudo apt-get install kubectl 
-   
-   # install gcloud auth plugin using this command
-   sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
-   
-   # log in to your cluster
-   gcloud container clusters get-credentials "$CLUSTER_NAME" --region "$CLUSTER_REGION" --project "$PREJECT_NAME"
-   
    # Log in to Docker Hub
    gcloud auth print-access-token | sudo docker login -u oauth2accesstoken --password-stdin $REGION-docker.pkg.dev
 
    # Pull and push the images
-   sudo docker pull petro8/node-app
-   sudo docker tag docker.io/petro8/node-app:latest $REGION-docker.pkg.dev/$PROJECT_NAME/$REPO_NAME/petro8/node-app:v1
-   sudo docker push $REGION-docker.pkg.dev/$PROJECT_NAME/$REPO_NAME/petro8/node-app:v1
+   sudo docker pull fadyelyas/hello-app:latest
+   sudo docker tag docker.io/fadyelyas/hello-app:latest $REGION-docker.pkg.dev/$PROJECT_NAME/$REPO_NAME/fadyelyas/hello-app:latest
+   sudo docker push $REGION-docker.pkg.dev/$PROJECT_NAME/$REPO_NAME/fadyelyas/hello-app:latest
    
    
    ############
