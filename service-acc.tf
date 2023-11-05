07e10c55db47b34c4458aae84a519358221a0636
FadyAtefElyas <fadyatefelyas@gmai.com>
2023-11-05 08:36:26 +0200

    modify the infra

--- service-acc.tf
+++ service-acc.tf
@@ -1,19 +1,3 @@
-# the management-instance -service -account
-resource "google_service_account" "kubernetes_engine_developer" {
-  account_id   = var.account_id
-  display_name = var.account_display_name
-}
-resource "google_project_iam_member" "kubernetes_engine_developer_role" {
-  project = google_service_account.kubernetes_engine_developer.project
-  role    = "roles/artifactregistry.writer"
-  member  = "serviceAccount:${google_service_account.kubernetes_engine_developer.email}"
-}
-resource "google_project_iam_member" "kubernetes_engine_developer_role2" {
-  project = google_service_account.kubernetes_engine_developer.project
-  role    = "roles/container.admin"
-  member  = "serviceAccount:${google_service_account.kubernetes_engine_developer.email}"
-}
-
 # the k8s cluster service acc
 
 resource "google_service_account" "kubernetes_cluster_sa" {
@@ -27,24 +11,4 @@ resource "google_project_iam_member" "my_service_account_roles" {
   member  = "serviceAccount:${google_service_account.kubernetes_cluster_sa.email}"
 }
 
-/* resource "google_project_iam_custom_role" "custom_role" {
-     role_id   = var.custom_role_id
-     title     = var.custom_role_title
-     stage     = "GA"
-     permissions = [
-       "container.pods.attach",
-       "container.pods.create",
-       "container.pods.delete",
-       "container.pods.get",
-       "container.pods.getLogs",
-       "container.pods.getStatus",
-       "container.pods.initialize",
-       "container.pods.list",
-       "container.pods.portForward",
-       "container.pods.proxy",
-       "container.pods.update",
-       "container.pods.updateStatus",
-       "container.cluster.get",
-       "container.cluster.list"
-     ]
-   }*/
+
